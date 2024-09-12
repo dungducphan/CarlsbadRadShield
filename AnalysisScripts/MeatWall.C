35e53aad41dad5fd4818940f576c446f3714a0be
Dung D. Phan <brianp.dung@utexas.edu>
2024-09-12 10:18:58 -0500

    Adding analysis for the Meat Wall case

--- AnalysisScripts/MeatWall.C
+++ AnalysisScripts/MeatWall.C
@@ -10,13 +10,13 @@
 #include <iostream>
 
 const std::string DataPath = "/home/dphan/Documents/GitHub/CarlsbadRadShield/AnalysisScripts/";
-const std::string DataFile = "EDep_001_8800000_Iso_10MeV_HDC3_FE1_HDC12.root";
+const std::string DataFile = "EDep_002_88000000_Iso_1MeV_HDC18.root";
 const std::string PlotPath = "/home/dphan/Documents/GitHub/CarlsbadRadShield/AnalysisScripts/Plots/";
-const double massOfPhantom = 168.117; // kg
+const double massOfPhantom = 1.03; // kg
 const double scaleFactor = 7102.3; // scale factor to the targeted charge
 const double laserReprate = 100; // Hz
 
-class BeamAna {
+class MeatWall {
 public :
    TTree          *fChain;   //!pointer to the analyzed TTree or TChain
    Int_t           fCurrent; //!current Tree number in a TChain
@@ -25,16 +25,14 @@ public :
 
    // Declaration of leaf types
    Double_t        EDep;
-   Double_t        PDGID;
-   Double_t        HumanPhantom_ID;
+   Int_t           Phantom_ID;
 
    // List of branches
    TBranch        *b_EDep;   //!
-   TBranch        *b_PDGID;   //!
-   TBranch        *b_HumanPhantom_ID;   //!
+   TBranch        *b_Phantom_ID;   //!
 
-   BeamAna(TTree *tree=0);
-   virtual ~BeamAna();
+   MeatWall(TTree *tree=0);
+   virtual ~MeatWall();
    virtual Int_t    Cut(Long64_t entry);
    virtual Int_t    GetEntry(Long64_t entry);
    virtual Long64_t LoadTree(Long64_t entry);
@@ -44,7 +42,7 @@ public :
    virtual void     Show(Long64_t entry = -1);
 };
 
-BeamAna::BeamAna(TTree *tree) : fChain(0) {
+MeatWall::MeatWall(TTree *tree) : fChain(0) {
 // if parameter tree is not specified (or zero), connect the file
 // used to generate this class and read the Tree.
    if (tree == 0) {
@@ -58,18 +56,18 @@ BeamAna::BeamAna(TTree *tree) : fChain(0) {
    Init(tree);
 }
 
-BeamAna::~BeamAna() {
+MeatWall::~MeatWall() {
    if (!fChain) return;
    delete fChain->GetCurrentFile();
 }
 
-Int_t BeamAna::GetEntry(Long64_t entry)
+Int_t MeatWall::GetEntry(Long64_t entry)
 {
 // Read contents of entry.
    if (!fChain) return 0;
    return fChain->GetEntry(entry);
 }
-Long64_t BeamAna::LoadTree(Long64_t entry)
+Long64_t MeatWall::LoadTree(Long64_t entry)
 {
 // Set the environment to read one entry
    if (!fChain) return -5;
@@ -82,7 +80,7 @@ Long64_t BeamAna::LoadTree(Long64_t entry)
    return centry;
 }
 
-void BeamAna::Init(TTree *tree) {
+void MeatWall::Init(TTree *tree) {
    // Set branch addresses and branch pointers
    if (!tree) return;
    fChain = tree;
@@ -90,38 +88,34 @@ void BeamAna::Init(TTree *tree) {
    fChain->SetMakeClass(1);
 
    fChain->SetBranchAddress("EDep", &EDep, &b_EDep);
-   fChain->SetBranchAddress("PDGID", &PDGID, &b_PDGID);
-   fChain->SetBranchAddress("HumanPhantom_ID", &HumanPhantom_ID, &b_HumanPhantom_ID);
+   fChain->SetBranchAddress("HumanPhantom_ID", &Phantom_ID, &b_Phantom_ID);
    Notify();
 }
 
-Bool_t BeamAna::Notify() {
+Bool_t MeatWall::Notify() {
    return kTRUE;
 }
 
-void BeamAna::Show(Long64_t entry) {
+void MeatWall::Show(Long64_t entry) {
 // Print contents of entry.
 // If entry is not specified, print current entry
    if (!fChain) return;
    fChain->Show(entry);
 }
 
-Int_t BeamAna::Cut(Long64_t entry) {
+Int_t MeatWall::Cut(Long64_t entry) {
 // This function may be called from Loop.
 // returns  1 if entry is accepted.
 // returns -1 otherwise.
    return 1;
 }
 
-void BeamAna::Loop() {
+void MeatWall::Loop() {
    if (fChain == 0) return;
 
    Long64_t nentries = fChain->GetEntriesFast();
 
-   double absorbedEnergy_Human1 = 0; // Joule
-   double absorbedEnergy_Human2 = 0; // Joule
-   double absorbedEnergy_Human3 = 0; // Joule
-   double absorbedEnergy_Human4 = 0; // Joule
+   TH2D* meatWall = new TH2D("meatWall", "Meat Wall", 31, -0.5, 30.5, 31, -0.5, 30.5);
 
    Long64_t nbytes = 0, nb = 0;
    for (Long64_t jentry=0; jentry<nentries;jentry++) {
@@ -129,31 +123,20 @@ void BeamAna::Loop() {
       if (ientry < 0) break;
       nb = fChain->GetEntry(jentry);   nbytes += nb;
       // if (Cut(ientry) < 0) continue;
-
-      if (HumanPhantom_ID == 1) {
-         absorbedEnergy_Human1 += EDep;
-      } else if (HumanPhantom_ID == 2) {
-         absorbedEnergy_Human2 += EDep;
-      } else if (HumanPhantom_ID == 3) {
-         absorbedEnergy_Human3 += EDep;
-      } else if (HumanPhantom_ID == 4) {
-         absorbedEnergy_Human4 += EDep;
-      }
+      int x = Phantom_ID / 100;
+      int y = Phantom_ID % 100;
+      meatWall->Fill(x, y, EDep);
    }
 
-   absorbedEnergy_Human1 = absorbedEnergy_Human1 * scaleFactor * laserReprate / massOfPhantom; // Gy/s
-   absorbedEnergy_Human2 = absorbedEnergy_Human2 * scaleFactor * laserReprate / massOfPhantom; // Gy/s
-
-   // std::cout << "Beam Charge: 50pC. Rep-rate 100Hz. Beam Energy 200MeV." << std::endl;
-   std::cout << "Diffuse Charge: 10nC. Rep-rate 100Hz. Electron Energy 10MeV." << std::endl;
-   std::cout << "Absorbed Dose Rate in Human Phantom 1: " << absorbedEnergy_Human1 * 100 * 1000 << " mrem/s" << std::endl;
-   std::cout << "Absorbed Dose Rate in Human Phantom 2: " << absorbedEnergy_Human2 * 100 * 1000 << " mrem/s" << std::endl;
+   meatWall->Scale(scaleFactor * laserReprate / massOfPhantom);
+   meatWall->Draw("colz");
+   gStyle->SetOptStat(0);
 }
 
 #if !defined(__CINT__) && !defined(__CLING__) && !defined(__ACLIC__)
 
 int main() {
-   BeamAna *ana = new BeamAna();
+   MeatWall *ana = new MeatWall();
    ana->Loop();
 
    return 0;
