e20392500708b762a5cbc5c6324aa8d008dde60d
Dung D. Phan <brianp.dung@utexas.edu>
2025-02-27 09:09:40 -0600

    Update runaction.cc

--- G4Sim/src/runaction.cc
+++ G4Sim/src/runaction.cc
@@ -3,16 +3,19 @@
 runAction::runAction() : G4UserRunAction() {
     man = G4AnalysisManager::Instance();
 
+    // Control room
     man->CreateH2("DoseMap_ControlRoom", "Dose Map in Control Room",
                   100, 0., 100., // x-axis: 100 bins from 0 to 100
                   10, 0., 10.  // y-axis: 100 bins from 0 to 100
     );
 
+    // Parking Area
     man->CreateH2("DoseMap_ParkingArea", "Dose Map in Parking Area",
                   150, 0., 150., // x-axis: 100 bins from 0 to 100
                   10, 0., 10.  // y-axis: 100 bins from 0 to 100
     );
 
+    // Lunch Area
     man->CreateH2("DoseMap_LunchArea", "Dose Map in Lunch Area",
                   100, 0., 100., // x-axis: 100 bins from 0 to 100
                   10, 0., 10.  // y-axis: 100 bins from 0 to 100
