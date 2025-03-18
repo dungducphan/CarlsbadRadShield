ef30a76559ed821a865e5d0ae0a121e9900b5e52
Dung D. Phan <brianp.dung@utexas.edu>
2025-03-18 10:30:30 -0500

    Fix energy and viewpoint

--- G4Sim/src/generator.cc
+++ G4Sim/src/generator.cc
@@ -19,7 +19,7 @@ generator::generator() {
     sps->GetPosDist()->ConfineSourceToVolume("NULL");
 
     sps->GetEneDist()->SetEnergyDisType("Mono"); // Mono, Lin, Pow, Exp, Gaus, Brem, BBody, Cdg (cosmic diffuse gamma), User, Arb, Epn (energy per nucleon)
-    sps->GetEneDist()->SetMonoEnergy(100 * MeV);
+    sps->GetEneDist()->SetMonoEnergy(50 * MeV);
 
     fRandom = new TRandom3();
 }
@@ -34,7 +34,7 @@ void generator::GeneratePrimaries(G4Event *anEvent) {
 }
 
 G4ThreeVector generator::RandomizedDirection() {
-    double openingAngle = 0.1; // mrad
+    double openingAngle = 5; // mrad
     double rdm1 = fRandom->Gaus(0, openingAngle);
     double y_mom = TMath::Cos(rdm1 * mrad);
     double tmp = TMath::Sin(rdm1 * mrad);
