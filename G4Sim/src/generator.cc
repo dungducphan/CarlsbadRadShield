f8e2b1c5e1346ccc1f949dade9d8d19cbd9875bd
Dung D. Phan <brianp.dung@utexas.edu>
2025-03-21 10:15:21 -0500

    For demonstration and estimation purposes

--- G4Sim/src/generator.cc
+++ G4Sim/src/generator.cc
@@ -34,7 +34,7 @@ void generator::GeneratePrimaries(G4Event *anEvent) {
 }
 
 G4ThreeVector generator::RandomizedDirection() {
-    double openingAngle = 5; // mrad
+    double openingAngle = 0.1; // 10 mrad divergence // FIXME: check and change back before production
     double rdm1 = fRandom->Gaus(0, openingAngle);
     double y_mom = TMath::Cos(rdm1 * mrad);
     double tmp = TMath::Sin(rdm1 * mrad);
