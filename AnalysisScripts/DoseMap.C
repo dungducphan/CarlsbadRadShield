b10c277beac5fdd9d397f6e6094ae50631bba66a
Dung D. Phan <wrpninety@proton.me>
2025-09-18 13:13:14 -0500

    Adding results

--- AnalysisScripts/DoseMap.C
+++ AnalysisScripts/DoseMap.C
@@ -21,7 +21,7 @@
 int main() {
     gStyle->SetOptStat(0);
 
-    auto infile = new TFile("/home/dphan/Documents/GitHub/CarlsbadRadShield/Results/NoRoof_08222025/DoseMap_NoRoof_Beam50MeV.root");
+    auto infile = new TFile("/home/dphan/Documents/GitHub/CarlsbadRadShield/Results/NoRoof_09182025/DoseMap_NoRoof_Beam50MeV_BiggerWPinhole.root");
     auto h2_ControlRoom = (TH2D*)infile->Get("DoseMap_ControlRoom");
     auto h2_ParkingArea = (TH2D*)infile->Get("DoseMap_ParkingArea");
     auto h2_LunchArea   = (TH2D*)infile->Get("DoseMap_LunchArea");
@@ -33,8 +33,8 @@ int main() {
     /* Change these 2 values */
     /* ============================================================================== */
     double targetCharge = 15E-12; // C
-    double numberOfElectronsSimulated = 100000000;
-    bool scaleToFullWorkingYear = false;
+    double numberOfElectronsSimulated = 200000000;
+    bool scaleToFullWorkingYear = true;
     /* ============================================================================== */
 
     double massOfScoringBox = 8.24; // kg
@@ -67,7 +67,7 @@ int main() {
     h2_ControlRoom->GetYaxis()->CenterTitle();
     h2_ControlRoom->GetXaxis()->SetTitle("Cell Number");
     h2_ControlRoom->GetYaxis()->SetTitle("Cell Number");
-    c_ControlRoom->SaveAs("/home/dphan/Documents/GitHub/CarlsbadRadShield/Results/NoRoof_08222025/DoseMap_ControlRoom.png");
+    c_ControlRoom->SaveAs("/home/dphan/Documents/GitHub/CarlsbadRadShield/Results/NoRoof_09182025/DoseMap_ControlRoom.png");
 
     auto c_ParkingArea = new TCanvas("c_ParkingArea", "c_ParkingArea", 1800, 800);
     c_ParkingArea->SetMargin(0.1, 0.15, 0.15, 0.15);
@@ -79,7 +79,7 @@ int main() {
     h2_ParkingArea->GetYaxis()->CenterTitle();
     h2_ParkingArea->GetXaxis()->SetTitle("Cell Number");
     h2_ParkingArea->GetYaxis()->SetTitle("Cell Number");
-    c_ParkingArea->SaveAs("/home/dphan/Documents/GitHub/CarlsbadRadShield/Results/NoRoof_08222025/DoseMap_ParkingArea.png");
+    c_ParkingArea->SaveAs("/home/dphan/Documents/GitHub/CarlsbadRadShield/Results/NoRoof_09182025/DoseMap_ParkingArea.png");
 
     auto c_LunchArea = new TCanvas("c_LunchArea", "c_LunchArea", 1800, 800);
     c_LunchArea->SetMargin(0.1, 0.15, 0.15, 0.15);
@@ -91,7 +91,7 @@ int main() {
     h2_LunchArea->GetYaxis()->CenterTitle();
     h2_LunchArea->GetXaxis()->SetTitle("Cell Number");
     h2_LunchArea->GetYaxis()->SetTitle("Cell Number");
-    c_LunchArea->SaveAs("/home/dphan/Documents/GitHub/CarlsbadRadShield/Results/NoRoof_08222025/DoseMap_LunchArea.png");
+    c_LunchArea->SaveAs("/home/dphan/Documents/GitHub/CarlsbadRadShield/Results/NoRoof_09182025/DoseMap_LunchArea.png");
 
     auto c_OfficeArea = new TCanvas("c_OfficeArea", "c_OfficeArea", 1800, 800);
     c_OfficeArea->SetMargin(0.1, 0.15, 0.15, 0.15);
@@ -103,7 +103,7 @@ int main() {
     h2_OfficeArea->GetYaxis()->CenterTitle();
     h2_OfficeArea->GetXaxis()->SetTitle("Cell Number");
     h2_OfficeArea->GetYaxis()->SetTitle("Cell Number");
-    c_OfficeArea->SaveAs("/home/dphan/Documents/GitHub/CarlsbadRadShield/Results/NoRoof_08222025/DoseMap_OfficeArea.png");
+    c_OfficeArea->SaveAs("/home/dphan/Documents/GitHub/CarlsbadRadShield/Results/NoRoof_09182025/DoseMap_OfficeArea.png");
 
     auto c_LaserRoom = new TCanvas("c_LaserRoom", "c_LaserRoom", 1800, 800);
     c_LaserRoom->SetMargin(0.1, 0.15, 0.15, 0.15);
@@ -115,7 +115,7 @@ int main() {
     h2_LaserRoom->GetYaxis()->CenterTitle();
     h2_LaserRoom->GetXaxis()->SetTitle("Cell Number");
     h2_LaserRoom->GetYaxis()->SetTitle("Cell Number");
-    c_LaserRoom->SaveAs("/home/dphan/Documents/GitHub/CarlsbadRadShield/Results/NoRoof_08222025/DoseMap_LaserRoom.png");
+    c_LaserRoom->SaveAs("/home/dphan/Documents/GitHub/CarlsbadRadShield/Results/NoRoof_09182025/DoseMap_LaserRoom.png");
 
     return 0;
 }
